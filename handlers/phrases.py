2b7f128a79f1c0136a16c14dc36c807b617c965e
Makaveli396 <underom96@gmail.com>
2025-07-18 20:41:40 -0600

    Add files via upload

--- handlers/phrases.py
+++ handlers/phrases.py
@@ -1,6 +1,10 @@
 from telegram import Update
 
 async def phrase_middleware(update: Update, context):
+    if update.message:
+        print(f"[DEBUG] Mensaje recibido: {update.message.text}")
+        await update.message.reply_text("📨 Mensaje recibido")
+
     text = update.message.text if update.message and update.message.text else ""
     if "cine" in text.lower():
         await update.message.reply_text("🎥 ¡Viva el buen cine!")
